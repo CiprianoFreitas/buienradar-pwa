6964e0524ff0c55062d9468d7448625d9c41913b
Cipriano Freitas <casf1992@gmail.com>
2019-06-17 20:03:22 +0200

    different strategy for cors

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS
 from buienradar.buienradar import (get_data, parse_data)
 from buienradar.constants import (CONTENT, RAINCONTENT, SUCCESS)
 
 app = Flask(__name__)
-CORS(app)
+
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+        headers = request.headers.get('Access-Control-Request-Headers')
+        if headers:
+            response.headers['Access-Control-Allow-Headers'] = headers
+    return response
+app.after_request(add_cors_headers)
 
 def parse_rainfall(data):
     lines = data.splitlines()
